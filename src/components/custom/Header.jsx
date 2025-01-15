5d050e2c05be23209c9f87834eeced5b7ff57a7c
Wasif-Ansari <110668035+Wasif-Ansari@users.noreply.github.com>
2025-01-16 03:02:26 +0530

    Update Header.jsx

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -3,17 +3,27 @@ import { Button } from '../ui/button'
 import DarkModeToggle from './DarkModeToggle'
 
 const Header = () => {
+
+  const user = JSON.parse(localStorage.getItem('user'))
+  console.log("User", user)
+
   return (
     <div className='flex justify-between items-center p-3 bg-violet-600'>
       <div className='flex flex-row gap-3 items-center px-2'>
-        <img className='p-2 shadow-sm bg-white rounded-2xl w-13 h-12' src='/travel-svgrepo-com.svg'/>
+        <img className='p-2 shadow-sm bg-white rounded-2xl w-13 h-12' src='/travel-svgrepo-com.svg' />
         <h2 className='text-white font-bold text-2xl'>TravelBlueprint.ai </h2>
-        </div>
+      </div>
 
-        <div className='flex flex-row gap-3'>
-        <Button>Sign in</Button>
-        <DarkModeToggle/>
-        </div>
+      <div className='flex flex-row gap-3 items-center'>
+        {user ?
+          <div className='flex items-center'>
+            <img className='h-10 w-10 rounded-full mx-2' src={user?.picture} />
+            <h2 className='text-white text-md font-bold'>{user?.name}</h2>
+          </div>
+          : <Button>Sign in</Button>
+        }
+        <DarkModeToggle />
+      </div>
     </div>
   )
 }
